d2cd35e7371a7239b745afb73f8cd8f1b49613ef
Tim Shaker <tim@tmshkr.com>
2020-07-06 15:57:14 -0700

    complete 14_cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,27 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+d = datetime.now()
+l = len(sys.argv)
+
+if l == 3:
+    print(calendar.month(int(sys.argv[2]), int(sys.argv[1])))
+elif l == 2:
+    print(calendar.month(d.year, int(sys.argv[1])))
+elif l == 1:
+    print(calendar.month(d.year, d.month))
+else:
+    print("Expected format: 14_cal.py [month] [year]")
